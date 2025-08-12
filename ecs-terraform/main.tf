4118d60ac9514f6a00855dfdf7ec6fb37f8ebc22
Abarna Lakshmi <abarnalakshmir@Abarnas-MacBook-Air.local>
2025-08-12 17:07:04 +0530

    Updated ALB

--- ecs-terraform/main.tf
+++ ecs-terraform/main.tf
@@ -269,24 +269,3 @@ resource "aws_ecs_service" "frontend" {
     aws_lb_listener.http
   ]
 }
-
-# --- Outputs ---
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.this.name
-}
-
-output "frontend_service_arn" {
-  value = aws_ecs_service.frontend.arn
-}
-
-output "backend_service_arn" {
-  value = aws_ecs_service.backend.arn
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "alb_dns_name" {
-  value = aws_lb.app_alb.dns_name
-}
