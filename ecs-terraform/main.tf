37ee81fb26ab1ca662694e28c29b94b88e648cfb
Abarna Lakshmi <abarnalakshmir@Abarnas-MacBook-Air.local>
2025-08-19 10:43:02 +0530

    updated commit

--- ecs-terraform/main.tf
+++ ecs-terraform/main.tf
@@ -110,24 +110,24 @@ data "aws_iam_role" "ecs_task_execution" {
 }
 
 # Alternatively, comment out the below resource if role exists:
-# resource "aws_iam_role" "ecs_task_execution" {
-#   name = "ecsTaskExecutionRole-simple"
-#   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role.json
-# }
-# data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ecs-tasks.amazonaws.com"]
-#     }
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
-# resource "aws_iam_role_policy_attachment" "task_exec_attach" {
-#   role       = aws_iam_role.ecs_task_execution.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-# }
+ resource "aws_iam_role" "ecs_task_execution" {
+   name = "ecsTaskExecutionRole-simple"
+   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role.json
+ }
+ data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
+   statement {
+     effect = "Allow"
+     principals {
+       type        = "Service"
+       identifiers = ["ecs-tasks.amazonaws.com"]
+     }
+     actions = ["sts:AssumeRole"]
+   }
+ }
+ resource "aws_iam_role_policy_attachment" "task_exec_attach" {
+   role       = aws_iam_role.ecs_task_execution.name
+   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+ }
 
 # --- ECS Cluster ---
 resource "aws_ecs_cluster" "this" {
